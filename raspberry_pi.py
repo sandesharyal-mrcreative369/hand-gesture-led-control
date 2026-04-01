34bdf4f6beaf32daf396174f40844b1c97f13bcc
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-04-01 23:26:08 +0530

    Camera Setup Code in Raspberry Pi

--- raspberry_pi.py
+++ raspberry_pi.py
@@ -1,1 +1,20 @@
 
+from picamera2 import Picamera2
+import cv2
+
+picam2 = Picamera2()
+
+
+picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480)}))
+
+picam2.start()
+
+while True:
+    frame = picam2.capture_array()
+
+    cv2.imshow("Pi Camera (Bullseye)", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
