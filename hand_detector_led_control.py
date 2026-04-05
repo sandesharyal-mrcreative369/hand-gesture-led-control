7011e0655d04f6dd22fcb44a63136d4bb5d5ba14
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-04-05 23:44:37 +0530

    Final lookup of Project

--- hand_detector_led_control.py
+++ hand_detector_led_control.py
@@ -3,99 +3,82 @@ import cv2
 import time
 import math
 
-top_points=[4,8,12,16,20]
-mid_points=[3,6,10,14,18]
 
-total_points=[]
-fingers=[]
+class handDetection:
 
-myHands = mp.solutions.hands
-hands =myHands.Hands()
-drawLandMarks = mp.solutions.drawing_utils
+    def __init__(self):
 
-current_Time = 0
-present_Time = 0
-video = cv2.VideoCapture(0)
+        self.myHands = mp.solutions.hands
+        self.hands = self.myHands.Hands()
+        self.drawLandMarks = mp.solutions.drawing_utils
 
-x4=y4=x8=y8=x0=y0=x12=y12=x16=y16=x20=y20 =0
+        self. current_Time = 0
+        self.present_Time = 0
 
-while True:
-    success, frame = video.read()
-    frame = cv2.flip(frame,1)
-    RGB_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    if not success:
-         print("Failed to receive a frame")
-         continue
+        self.top_points = [4, 8, 12, 16, 20]
+        self.mid_points = [3, 6, 10, 14, 18]
 
-    results = hands.process(RGB_image)
-    # global distance4,distance8,distance12,distance16,distance20
+        self.total_points=[]
+        self.fingers = []
 
-    if results.multi_hand_landmarks:
-        total_points = []  # Reset Frame
-        fingers = [] #Reset Frame
-        for handLMS in results.multi_hand_landmarks:
-          for id,lms in enumerate(handLMS.landmark):
-              h,w,c=frame.shape
-              tw ,th = int((lms.x*w)) , int((lms.y*h))
+    def hand_draw(self,frame):
+        global fingersCount
+        while True:
+            frame = cv2.flip(frame,1)
+            RGB_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-              total_points.append([tw,th])
+            self.current_Time = time.time()
+            fps = 1 / (self.current_Time - self.present_Time)
+            self.present_Time = self.current_Time
 
+            cv2.putText(frame, str(int(fps)), (40, 80), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+            cv2.imshow("Frame", frame)
 
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-          drawLandMarks.draw_landmarks(frame,handLMS,myHands.HAND_CONNECTIONS)
+            results = self.hands.process(RGB_image)
 
-            #Thumb Case
-          if total_points[4][0] < total_points[3][0]:
-            fingers.append(1)  # Open
+            if results.multi_hand_landmarks:
+                total_points = []  # Reset Frame
+                fingers = [] #Reset Frame
+                for handLMS in results.multi_hand_landmarks:
+                  for id,lms in enumerate(handLMS.landmark):
+                      h,w,c=frame.shape
+                      tw ,th = int((lms.x*w)) , int((lms.y*h))
 
-          else:
-            fingers.append(0)  # Closed
+                      total_points.append([tw,th])
 
+                  self.drawLandMarks.draw_landmarks(frame,handLMS,self.myHands.HAND_CONNECTIONS)
 
-        # Others fingers
-          for i in range(1,5):
-             if total_points[top_points[i]][1] < total_points[mid_points[i]][1]:
-                fingers.append(1)
-             else:
-                fingers.append(0)
+                cv2.rectangle(frame, (40, 70), (100, 200), (0, 255, 0), cv2.FILLED)
+                cv2.putText(frame, str(int(fingersCount)), (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-          fingersCount = fingers.count(1)
-          print(fingersCount)
+        self.video.release()
+        cv2.destroyAllWindows()
 
-          cv2.rectangle(frame,(40,70),(100,200),(0,255,0),cv2.FILLED)
-          cv2.putText(frame,str(int(fingersCount)),(50,150),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-        # if fingers == [1, 0, 0, 0, 0]:
-        #     print("1 → Thumb")
-        #
-        # elif fingers == [0, 1, 0, 0, 0]:
-        #     print("2 → Index")
-        #
-        # elif fingers == [0, 0, 1, 0, 0]:
-        #     print("3 → Middle")
-        #
-        # elif fingers == [0, 0, 0, 1, 0]:
-        #     print("4 → Ring")
-        #
-        # elif fingers == [0, 0, 0, 0, 1]:
-        #     print("5 → Pinky")
-        #
-        # elif fingers == [0, 0, 0, 0, 0]:
-        #     print("Close")
-        #
-        # else:
-        #     print("Open")
+                    #Thumb
+    def fingerCounter(self):
+      if self.total_points[4][0] < self.total_points[3][0]:
+        self.fingers.append(1)  # Open
+
+      else:
+        self.fingers.append(0)  # Closed
 
 
-    current_Time  = time.time()
-    fps = 1/(current_Time - present_Time)
-    present_Time = current_Time
+    # Others fingers
+      for i in range(1,5):
+         if self.total_points[self.top_points[i]][1] < self.total_points[self.mid_points[i]][1]:
+            self.fingers.append(1)
+         else:
+            self.fingers.append(0)
 
-    cv2.putText(frame, str(int(fps)), (40, 80),cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 2)
-    cv2.imshow("Frame",frame)
+      fingersCount = self.fingers.count(1)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+      return fingersCount
 
-video.release()
-cv2.destroyAllWindows()
 
+
+if __name__ == "__main__":
+    obj = handDetection()
+    obj.hand_draw(frame)
--- raspberry_pi.py
+++ raspberry_pi.py
@@ -1,20 +1,80 @@
 
 from picamera2 import Picamera2
 import cv2
+import RPi.GPIO as GPIO
+import time
+from hand_detector_led_control import handDetection
 
 picam2 = Picamera2()
-
-
 picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480)}))
-
 picam2.start()
 
-while True:
-    frame = picam2.capture_array()
+# Use BCM pin numbering
+GPIO.setmode(GPIO.BCM)
 
-    cv2.imshow("Pi Camera (Bullseye)", frame)
+LED_PIN0= 18
+LED_PIN1= 23
+LED_PIN2= 25
+LED_PIN3= 24
+LED_PIN4= 27
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+GPIO.setup(LED_PIN0, GPIO.OUT)
+GPIO.setup(LED_PIN1, GPIO.OUT)
+GPIO.setup(LED_PIN2, GPIO.OUT)
+GPIO.setup(LED_PIN3, GPIO.OUT)
+GPIO.setup(LED_PIN4, GPIO.OUT)
+
+
+detection = handDetection()
+try:
+    while True:
+
+        frame = picam2.capture_array()
+        detection.handDetect(frame)
+        counter = detection.fingerCounter()
+        cv2.imshow("Pi Camera (Bullseye)", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        if counter ==0:
+            GPIO.output(LED_PIN0, GPIO.HIGH)
+            print("LED ON")
+            time.sleep(1)
+
+        elif counter == 1:
+            GPIO.output(LED_PIN1, GPIO.HIGH)
+            print("LED ON")
+            time.sleep(1)
+
+        elif counter == 2:
+            GPIO.output(LED_PIN2, GPIO.HIGH)
+            print("LED ON")
+            time.sleep(1)
+
+        elif counter == 3:
+            GPIO.output(LED_PIN3, GPIO.HIGH)
+            print("LED ON")
+            time.sleep(1)
+
+        elif counter == 4:
+            GPIO.output(LED_PIN4, GPIO.HIGH)
+            print("LED ON")
+            time.sleep(1)
+        else:
+
+            GPIO.output(LED_PIN0, GPIO.LOW)
+            GPIO.output(LED_PIN1, GPIO.LOW)
+            GPIO.output(LED_PIN2, GPIO.LOW)
+            GPIO.output(LED_PIN3, GPIO.LOW)
+            GPIO.output(LED_PIN4, GPIO.LOW)
+            print("LED OFF")
+            time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Program stopped")
+
+finally:
+    GPIO.cleanup()
 
 cv2.destroyAllWindows()
